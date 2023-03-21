f2c527ce676f6b71ec9388386f1321820c638415
TheHighnote <ryanhighnote7@gmail.com>
2023-03-21 12:32:13 -0500

    added api call for getAllActivities and createRoutine

--- src/API-Adapter/index.js
+++ src/API-Adapter/index.js
@@ -1,23 +1,59 @@
-const URL = "https://fitnesstracker-p0or.onrender.com";
+const URL = "https://fitnesstracker-p0or.onrender.com/api";
 
 export const getAllPublicRoutines = async () => {
-  try {
-    const response = await fetch(`${URL}/api/routines`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const result = await response.json();
-    console.log(result, "API routines result");
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await fetch(`${URL}/routines`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const result = await response.json();
+        console.log(result, "API routines result");
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
 };
+export const createRoutine = async (name, goal, isPublic) => {
+    try {
+      const response = await fetch(`${URL}/routines`, {
+        method: "POST",
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          name: name,
+          goal: goal,
+          isPublic: isPublic
+        })
+      });
+      const result = await response.json();
+      console.log(result);
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  }
+export const getAllActivities = async () => {
+    try {
+      const response = await fetch(`${URL}/activities`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      const postArray = result.posts;
+  
+      return postArray;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 export const loginUser = async (username, password) => {
   try {
     console.log(`${URL}/users/login`);
-    const response = await fetch(`${URL}/api/users/login`, {
+    const response = await fetch(`${URL}/users/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -37,7 +73,7 @@ export const loginUser = async (username, password) => {
 };
 export const registerUser = async (username, password) => {
   try {
-    const response = await fetch(`${URL}/api/users/register`, {
+    const response = await fetch(`${URL}/users/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -53,3 +89,4 @@ export const registerUser = async (username, password) => {
     console.log(error);
   }
 };
+
