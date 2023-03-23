fae61298289b9c21aeff417823479c8b2cf40bac
TheHighnote <ryanhighnote7@gmail.com>
2023-03-23 11:58:33 -0500

    started on functionality for a "light mode"

--- src/components/DashButtons.jsx
+++ src/components/DashButtons.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, onClick } from "react-router-dom";
 
+// const lightmode = () => {
+//   let element = document.body
+//   element.classList.toggle("light-mode");
+// }
 const DashButtons = ({ routines, setRoutines }) => {
   console.log(routines, "Routines in RoutineList");
   return (
@@ -8,8 +12,9 @@ const DashButtons = ({ routines, setRoutines }) => {
       <div id="dashboardButtons">
         <Link to="/createroutine"><button className="material-symbols-outlined">post_add</button></Link>
         <Link to="/activitylist"><button className="material-symbols-outlined">fitness_center</button></Link>
-        <Link to="/createactivity"><button className="material-symbols-outlined">update</button></Link>
-        <button className="material-symbols-outlined">logout</button>
+        <Link to="/createactivity"><button className="material-symbols-outlined">logout</button></Link>
+        {/* <button onClick={lightmode()} className="material-symbols-outlined">light_mode</button> */}
+        <button className="material-symbols-outlined">light_mode</button>
         <img id="logoSideBar" src="/Untitled_Artwork 30.png" alt="" />
       </div>
     </div>
